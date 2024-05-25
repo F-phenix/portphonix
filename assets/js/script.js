fa3f86a4e39efcf447734b02bbcdb7b3428c59a8
c3k4ah <dominickp20.aps1b@gmail.com>
2024-05-25 22:24:44 +0300

    Fine , i'll do it myself .

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var headerPlaceholder = document.getElementById('header-placeholder');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'components/header.html', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            headerPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+};
